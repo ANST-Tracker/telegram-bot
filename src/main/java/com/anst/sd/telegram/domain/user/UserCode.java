1eba4960b1a1ab36c15aa182b5bf31e6628fd139
eridium <pestrikv1337@gmail.com>
2024-02-25 15:00:42 +0300

    Added Kafka path

--- src/main/java/com/anst/sd/telegram/domain/user/UserCode.java
+++ src/main/java/com/anst/sd/telegram/domain/user/UserCode.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserCode {
     @Id
     private String id;
